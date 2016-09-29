72e1ce66e36b928f698a200cd4afe50645a27e93
Jennifer L. Lucas <jenniferlynne.117@gmail.com>
2016-09-29 15:00:21 -0400

    add file

---
+++ src/TestArray.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class TestArray {
+	ArrayList<Wine> winelist = new ArrayList<Wine>();
+	
+	Winelist.add(new Wine(“Villa Alena”, “Moscato”, 6.99));
+	Winelist.add(new Wine(“Beviano”, “Mascato”, 9.99));
+	Winelist.add(new Wine(“Flip Flop”,  “Mascato”, 10.00));
+	Winelist.add(new Wine(“Columbia Valley”, ”Merlot”, 12.00));
+	Winelist.add(new Wine(“North Coast”, “Merlot”, 9.99));
+	Winelist.add(new Wine(“Mark West”, “ Pinot Noir”, 11.00));
+	Winelist.add(new Wine(“DeLoach”, “Pinot Noir”, 18.99));
+	Winelist.add(new Wine(“Castle Rock”, ”Pinot Noir”, 8.99));
+	Winelist.add(new Wine(“Murphy’s Law”, “Riesling”, 12.99));
+	Winelist.add(new Wine(“Washington Hills”, “Riesling”, 10.00));
+	Winelist.add(new Wine(“Standing Stone”, “Riesling”, 11.99));
+	Winelist.add(new Wine(“Bex”, “Riesling”, 10.99));
+
+
+}
