95ab0fb270f2db3562e43fa5d2d189ff377afd3b
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-29 14:39:45 -0400

    name,type, price

---
+++ src/ProductManager.java
@@ -0,0 +1,74 @@
+public class ProductManager {
+	private String name; 
+	private String type; 
+	private String description; 
+	private double price; 
+	private static double tax = 6.00;
+	
+	//constructor 
+	public Product(String name, String category, String description, double price){
+		this.name = name;
+		this.type = category;
+		this.description = description;
+		this.price = price;
+	}
+	
+	public Product(String name, String category, String description, String price){
+		this.name = name;
+		this.type = category;
+		this.description = description;
+		this.price = Double.parseDouble(price);
+		}
+	
+	public Product(){
+		this.name = null;
+		this.type = null;
+		this.description = null;
+		this.price = 0;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getCategory() {
+		return type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public static double getTax() { 
+		return tax;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setCategory(String category) {
+		this.type = type;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public void setPrice(String details) {
+		this.price = Double.parseDouble(details);
+	}
+	
+	public String toString() {
+		return name + "(" + type + ")" + description + price;
+	}
+}
