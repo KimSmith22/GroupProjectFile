fbb20300c60306959d0709a620e51f8b4fd25f15
Jennifer L. Lucas <jenniferlynne.117@gmail.com>
2016-09-30 10:18:33 -0400

    added methods

--- src/POSTerminalApp.java
+++ src/POSTerminalApp.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 
 /**
  * 
@@ -17,23 +19,42 @@ public class POSTerminalApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Wine wine = new Wine();
 		// variables
 		String choice = "y";
-ArrayList<Wine> winelist = new ArrayList<Wine>();
-	
-	winelist.add(new Wine("Villa Alena", "Moscato", 6.99));
+		ArrayList<Wine> winelist = new ArrayList<Wine>();
+	//String[] items = winelist.split(",");
+	winelist.add(new Wine("Villa Alena",  "Moscato", 6.99));
 	winelist.add(new Wine("Beviano", "Mascato", 9.99));
 	winelist.add(new Wine("Flip Flop",  "Mascato", 10.00));
 	winelist.add(new Wine("Columbia Valley", "Merlot", 12.00));
-	winelist.add(new Wine("North Coast", "Merlot", 9.99));
+	winelist.add(new Wine("North Coast", "Merlot", 18.00));
 	winelist.add(new Wine("Mark West", "Pinot Noir", 11.00));
-	winelist.add(new Wine("DeLoach", "Pinot Noir", 18.99));
-	winelist.add(new Wine("Castle Rock", "Pinot Noir", 8.99));
-	winelist.add(new Wine("Murphy’s Law", "Riesling", 12.99));
-	winelist.add(new Wine("Washington Hills", "Riesling", 10.00));
-	winelist.add(new Wine("Standing Stone", "Riesling", 11.99));
-	winelist.add(new Wine("Bex", "Riesling", 10.99));
-
+	winelist.add(new Wine("DeLoach", "Pinot Noir", 8.99));
+	winelist.add(new Wine("Castle Rock", "Pinot Noir", 12.99));
+	winelist.add(new Wine("Murphy’s Law", "Riesling", 11.99));
+	winelist.add(new Wine("Washington Hills", "Riesling", 7.99));
+	winelist.add(new Wine("Standing Stone", "Riesling", 6.99));
+	winelist.add(new Wine("Bex", "Riesling", 5.99));
+	System.out.printf("Wine Name\t Type \t Price\n");
+	for (Wine x: winelist) {				
+						System.out.println(x);
+	}		
+				/*	
+				List<Double> Price= new ArrayList<Double>();
+ Price.add(6.99);
+ Price.add(9.99); 
+ Price.add(10.00);
+ Price.add(12.00);
+ Price.add(18.00);
+ Price.add(11.00);
+ Price.add(8.99);
+ Price.add(12.99);
+ Price.add(11.99);
+ Price.add(7.99);
+ Price.add(6.99);
+ Price.add(5.99);
+ */
 		// welcome and prompts
 		System.out.println("Welcome to our Wine Shop!");
 		System.out.println();
--- src/Wine.java
+++ src/Wine.java
@@ -5,23 +5,50 @@ private String name;
 private String type ;
 private double price;
  
-	public Wine(String name, String type, double price) {// constructor
-
+public Wine (){
+	
+}
+	public Wine(String name, String type, double price) {// constructor. passing local variables
+		this.name = name;//setting the variables using (this)
+		this.type = type;
+		this.price = price;
 	}
-
-	public  String getname(){
-		return name;
+	// creating getters and setters
+	
+	
+	public String getName(){
+		return  this.name;
+	}
+	
+	public String getName(String newName){
+		return (this.name = newName);
+	}
+	
+	
+		public String getType(){
+		return this.type;
+		}
+	
+		public String getType(String newType){
+			return (this.type = newType);
+		}
+		public double getPrice(){
+		return this.price;
+		}
 		
+		public double setPrice(double newPrice){
+			return (this.price = newPrice);
+		}
+	
+		public String toString(){
+        return  name  + type + price;
+    }
+	 
+	
 	}
+	
 
-	public void gettype(){
-
-}
-
-	public double getprice() {
-
-		return price ;
-
-	}
-
-}
+	
+	
+	
+	
