9829403b0e7a6704b26d0deda99e385f5bf44bbc
Jennifer L. Lucas <jenniferlynne.117@gmail.com>
2016-10-01 14:09:15 -0400

    updated file

--- src/WineGui.java
+++ src/WineGui.java
@@ -1,57 +1,54 @@
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Scanner;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 
 public class WineGui extends JFrame implements ActionListener {
-JButton b1;
-JButton b2;
-public WineGui(){
-	super();	
-	JOptionPane.showMessageDialog(null,"Please make a selection","Welcome to the Wine Shop!", JOptionPane.PLAIN_MESSAGE);
-
-	setSize(600, 400);
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setLookAndFeel();
-	
-	b1 = new JButton("Enter");
-	b2 = new JButton("Exit");
-	b1.addActionListener(this);
-    b2.addActionListener(this);
-	FlowLayout flow = new FlowLayout();
-	setLayout(flow);
-	add(b1);
-	add(b2);
-	pack();
-	setVisible(true);
-}
-
-public void actionPerformed(ActionEvent evt){
-	Object source = evt.getSource();
-	if(source == b1){
-		setTitle("Enter");
-	}else if(source == b2){
-		setTitle("Exit");
-	}
-}
-
-private void setLookAndFeel(){
-try{
-	UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-	SwingUtilities.updateComponentTreeUI(this);
-}catch (Exception exc) {System.err.println("Couldn't use the system" + "look and feel:" + exc); }
-}
-public static void main(String[]arguments){
-	WineGui frame = new WineGui();
-    }
-}
-
 
 	
+	private JButton Pinot_Noir, Merlot , Reisling, Moscato;
+	
+	public static void main(String[] args) {
+	WineGui gui = new WineGui();
+	JTable winegui = new JTable();
+	
+		gui.setVisible(true);	// making the gui program visible
 
+		
+	}//end of main method
+    
+    
+	public WineGui() {
+		
+		setTitle("The Wine Shop");
+		setSize(400, 400);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		// Add GUI components to let the user pick what type of wine they want.
+		Pinot_Noir = new JButton("Pinot Noir");
+		Merlot = new JButton("Merlot");
+		Reisling = new JButton("Reisling");
+		Moscato = new JButton("Moscato");
+		
+		Pinot_Noir.addActionListener(this);
+		Merlot.addActionListener(this);
+		Reisling.addActionListener(this);
+		Moscato.addActionListener(this);
 
+		Box buttons = Box.createHorizontalBox();
+		buttons.add(Pinot_Noir);
+		buttons.add(Merlot);
+		buttons.add(Reisling);
+		buttons.add(Moscato);
+		this.getContentPane().add(buttons, BorderLayout.CENTER);
+		
+		//JTable table = new JTable(winelist, receipt);
+		
+    }//end of constructor
+    
+    public void actionPerformed(ActionEvent e) {
+     
+    }//end of actionPerformed method
+    
+}
