eed289bd009a1c14fe46b57895cf50bb9c870668
Jennifer L. Lucas <jenniferlynne.117@gmail.com>
2016-10-02 17:33:07 -0400

    updated file

--- src/WineGui.java
+++ src/WineGui.java
@@ -1,10 +1,16 @@
+import java.awt.Color;
+import java.awt.Label;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 public class WineGui extends JFrame {
 
 	//creating the jframe
+	JLabel label;
+	
 	JTable jt;
 	String[] column_Headers={"Product Names","Product Type","Prices"};
 	String[][] product_Info={ {"Villa Alena", "Moscato","6.99"},
@@ -21,18 +27,24 @@ public class WineGui extends JFrame {
             {"Bex", "Riesling","5.99"}};
 	
 			public WineGui() {
-			}
-			{
+			
+			
+					
+
 				jt = new JTable(product_Info,column_Headers);
-				jt.setBounds(50,50,200,230);
+				jt.setBounds(50,50,100,130);
+				jt.setForeground(new Color(128, 0, 255));// setting the color for the text
 				JScrollPane js = new JScrollPane(jt);
 				this.add(js);//add jscroll to the table
 				this.setSize(500,400);
 				this.setVisible(true);
+				
+				label=new JLabel("The Wine Shop");
 			}
 	public static void main(String[]args) {
 		
 new WineGui();// instance of the class
+
 	}
 
 }
