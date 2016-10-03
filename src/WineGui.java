9d859da9aa1344871e1a5de55c8108be6411a249
Jennifer L. Lucas <jenniferlynne.117@gmail.com>
2016-10-03 08:03:57 -0400

    added scroll pane to JTable

--- src/WineGui.java
+++ src/WineGui.java
@@ -1,19 +1,24 @@
 import java.awt.Color;
-import java.awt.Label;
+import java.awt.Font;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
-public class WineGui extends JFrame {
-
-	//creating the jframe
-	JLabel label;
+public class WineGui {
 	
-	JTable jt;
-	String[] column_Headers={"Product Names","Product Type","Prices"};
-	String[][] product_Info={ {"Villa Alena", "Moscato","6.99"},
+    public static void main(String[] args){
+        
+        
+        JFrame frame = new JFrame();// creating a JFrame
+        JTable table = new JTable(); //Creating a JTable to store product names,types, and prices
+
+        // create a an array for the header of jtable
+        //created 2d array of strings thats being stored in the jtable
+       
+        Object[] columns = {"Product Name","Product Type","Price"};// created an array for the header of the JTable
+        
+        String[][] product_Info={ {"Villa Alena", "Moscato","6.99"},//created 2d array for the JTable
             {"Beviano", "Moscato","9.99" },
             {"Flip Flop",  "Moscato","9.99"},
             {"Columbia Valley", "Merlot","11.99"},
@@ -25,26 +30,30 @@ public class WineGui extends JFrame {
             {"Washington Hills", "Riesling","7.99"},
             {"Standing Stone", "Riesling","6.99"},
             {"Bex", "Riesling","5.99"}};
-	
-			public WineGui() {
-			
-			
-					
-
-				jt = new JTable(product_Info,column_Headers);
-				jt.setBounds(50,50,100,130);
-				jt.setForeground(new Color(128, 0, 255));// setting the color for the text
-				JScrollPane js = new JScrollPane(jt);
-				this.add(js);//add jscroll to the table
-				this.setSize(500,400);
-				this.setVisible(true);
-				
-				label=new JLabel("The Wine Shop");
-			}
-	public static void main(String[]args) {
-		
-new WineGui();// instance of the class
-
-	}
-
+        	
+      	table = new JTable(product_Info,columns);//constructor for the JTable
+        table.setBackground(Color.white);
+        table.setForeground(new Color(147, 112, 219));//color of the text
+        Font font = new Font("",1,22);
+        table.setFont(new Font("Calibri", Font.BOLD, 21));
+        table.setRowHeight(30);
+        table.setVisible(true);
+        
+       
+        // created JScrollPane for the JTable
+        // now able to scroll along the jtable
+        JScrollPane pane = new JScrollPane(table);
+        pane.setBounds(0, 0, 880, 200);
+        
+        frame.getContentPane().setLayout(null);
+        
+        frame.getContentPane().add(pane);
+        
+        frame.setSize(900,400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+    }
 }
+        
